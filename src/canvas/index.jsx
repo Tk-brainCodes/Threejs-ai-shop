9653ffef3086fd2bda80814e8e31094faf6176a9
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-04-11 13:47:53 +0100

    feat: rendered the glb model on the screen,and increased the field of view (fov) to 25, so the model can be enlarged

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,5 +1,24 @@
-const Canvas = () => {
-  return <div>Canvas</div>
+/* eslint-disable react/no-unknown-property */
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei'
+
+import CameraRig from './CameraRig'
+import Backdrop from './Backdrop'
+import Shirt from './Shirt'
+
+const CanvasModel = () => {
+  return (
+    <Canvas shadows camera={{ position: [0, 0, 0], fov: 25 }}>
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
+      <CameraRig>
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
+  )
 }
 
-export default Canvas
+export default CanvasModel
